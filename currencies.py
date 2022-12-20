eb6f12afbb9db03a659972cca42ddeb2a6b2b70b
jurmaev <smartemii03@gmail.com>
2022-12-20 12:03:55 +0500

    3.3.2 fix

--- currencies.py
+++ currencies.py
@@ -56,7 +56,7 @@ def convert_salary_to_rub(file_name):
             date = datetime.strptime(row['published_at'], '%Y-%m-%dT%H:%M:%S%z')
             df = pd.read_csv(f'csv_files/year_{date.year}.csv')
             convert_value = df[df['date'] == date.strftime('%Y-%m')][row['salary_currency']].values[0]
-            return math.isnan(convert_value) if 'NaN' else row['salary'] * convert_value
+            return 'NaN' if math.isnan(convert_value) else row['salary'] * convert_value
         return row['salary']
 
     def count_salary(row):
@@ -67,9 +67,9 @@ def convert_salary_to_rub(file_name):
         return (row['salary_from'] + row['salary_to']) / 2
 
     df = get_currency_dynamic(file_name)
-    df = df.head(10000)
+    df = df.head(1000)
     df['salary'] = df.apply(count_salary, axis=1)
-    df.apply(convert_to_rub, axis=1)
-    df.to_csv('salary_info.csv', index=False)
+    df['salary'] = df.apply(convert_to_rub, axis=1)
+    df.head(100).loc[:, ['name', 'salary', 'area_name', 'published_at']].to_csv('salary_info.csv', index=False)
 
 convert_salary_to_rub('vacancies_dif_currencies.csv')
